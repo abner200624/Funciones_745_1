f09de2995edc171d45968d9d2f6efb56fc445c60
abner200624 <asramosg@udistrital.edu.co>
2024-09-06 13:54:02 -0500

    V 1.2.1

--- src/com/sigmotoa/funcion_1/Funcion.java
+++ src/com/sigmotoa/funcion_1/Funcion.java
@@ -6,11 +6,12 @@ public class Funcion {
     public static void main(String[] args) {
         Scanner leer = new Scanner(System.in);
         saludar();
-        int resultao;
+        int resultao = 0;
         System.out.println("Hello, escribe tu nombre: ");
         String name = leer.next();
         System.out.println(name + " como estas por favor digita la opcion del menu que guste usar: ");
-        do {
+        int mennu;
+        do{
             System.out.println("1 - Suma de numeros");
             System.out.println("2 - Multiplicacion de numeros");
             System.out.println("3 - Potencia de numeros");
@@ -19,35 +20,58 @@ public class Funcion {
             System.out.println("6 - Calcular el area de un circulo");
             System.out.println("7 - Calcular el area de un cono");
             System.out.println("8 - Calcular la serie de Fibonacci n veces");
-            int mennu = leer.nextInt();
-            switch (mennu){
+            mennu = leer.nextInt();
+            int numa;int numb;
+            switch (mennu) {
                 case 1:
                     System.out.println("numero a sumarle:");
-                    int numa = leer.nextInt();
+                    numa = leer.nextInt();
                     System.out.println(name + " digita numero a ser sumado: ");
-                    int numb = leer.nextInt();
+                    numb = leer.nextInt();
                     resultao = sumita(numa, numb);
                     System.out.println("El resultado de su suma fue: " + resultao);
                     break;
                 case 2:
-                    System.out.println("Muy bien hecho, " + name + ", ahora digita el numero que deceas multiplicar: ");
-                    int numc = leer.nextInt();
+                    System.out.println(name + ", ahora digita el numero que deceas multiplicar: ");
+                    numa = leer.nextInt();
                     System.out.println(name + ", ahora digita el numero a multiplicar: ");
-                    int numd = leer.nextInt();
+                    numb = leer.nextInt();
                     resultao = multiplicasao(numa, numb);
                     System.out.println("El resultado de su multiplicacion fue: " + resultao);
+                    break;
+                case 3:
+                    System.out.println(name + ", ahora digita el numero que deceas potenciar: ");
+                    numa = leer.nextInt();
+                    System.out.println(name + ", ahora digita el exponente: ");
+                    numb = leer.nextInt();
+                    resultao = potencia(numa, numb);
+                    System.out.println("El resultado de su multiplicacion fue: " + resultao);
+                    break;
+                case 4:
+                    System.out.println(name + "Ingrese: ");
+                    break;
+                case 5:
+                    System.out.println(name + "Ingrese: ");
+                    break;
+                case 6:
+                    System.out.println(name + "Ingrese: ");
+                    break;
+                case 7:
+                    System.out.println(name + "Ingrese: ");
+                    break;
+                case 8:
+                    System.out.println(name + "Ingrese: ");
+                    break;
+            }
+        }while (mennu >= 9);
 
-        }
+        mayus (name);
 
-
-
-
-        mayus(name);
     }
 
     //Recibe, pero no retorna
-    public static void mayus(String nombre) {
-        System.out.println(nombre.toUpperCase());
+    public static void mayus(String name) {
+        System.out.println(name.toUpperCase());
     }
 
     //No Recibe, no retorna
@@ -70,15 +94,46 @@ public class Funcion {
     1. Sumar 2 numeros*/
 
     public static int sumita(int numa, int numb) {
-        return numa + numb;
+        return numa+numb;
     }
     /*
     2. Multiplicar 2 numeros con las sumas del 1
     */
 
+    public static int multiplicasao(int numa, int numb){
+        int multi = 0;
+        for (int i = 0; i < numb; i++)
+        multi = sumita(numa, multi);
+
+        return multi;
+    }
 
     /*
-    3. Potenciar 2 numeros con las sumas del 1
+        3. Potenciar 2 numeros con las sumas del 1
+        */
+    public static int potencia(int numa, int numb){
+        int poten=0;
+        for (int i = 0; i < numb; i++)
+            poten = multiplicasao(numa, poten);
+        return poten;
+    }
+
+/*
+    public class Arreglo {
+            int arreglo = leer.nextInt();
+            int promedio = 0;
+
+            for (int i = 0; i < arreglo.length; i++) {
+                System.out.println("ingrese la nota: " + (i + 1));
+                arreglo[i] = leer.nextInt();
+                promedio = promedio + arreglo[i];
+            }
+    }
+
+ */
+}
+
+/*
     4. Calcular el factorial de 1 numero
     5. Calcular el area de un cuadrado(recibiendo un lado)
     6. Calcular el area de un circulo(recibiendo el radio)
@@ -88,4 +143,3 @@ public class Funcion {
     Todas las funciones deben ser llamadas desde el Main.
      */
 
-}
